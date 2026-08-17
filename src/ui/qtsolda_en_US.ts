edb7b02fd9bd5f2c8177f260cad90037b06d65ed
Fábio Pereira da Silva <fabioegel@gmail.com>
2026-08-16 21:33:22 -0300

    Add about box

--- src/ui/qtsolda_en_US.ts
+++ src/ui/qtsolda_en_US.ts
@@ -5,32 +5,31 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="build/Desktop_Debug/qtsolda_autogen/include/ui_mainwindow.h" line="78"/>
-        <location filename="build/qtsolda_autogen/include/ui_mainwindow.h" line="51"/>
+        <location filename="build/Desktop_Debug/qtsolda_autogen/include/ui_mainwindow.h" line="76"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="27"/>
-        <location filename="build/Desktop_Debug/qtsolda_autogen/include/ui_mainwindow.h" line="80"/>
+        <location filename="build/Desktop_Debug/qtsolda_autogen/include/ui_mainwindow.h" line="78"/>
         <source>PushButton</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="50"/>
-        <location filename="build/Desktop_Debug/qtsolda_autogen/include/ui_mainwindow.h" line="81"/>
-        <source>CheckBox</source>
+        <location filename="mainwindow.ui" line="40"/>
+        <location filename="build/Desktop_Debug/qtsolda_autogen/include/ui_mainwindow.h" line="80"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:18pt; font-weight:700;&quot;&gt;Lista de técnicos&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;Lista técnicos, clientes e reparos&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="65"/>
+        <location filename="mainwindow.ui" line="55"/>
         <location filename="build/Desktop_Debug/qtsolda_autogen/include/ui_mainwindow.h" line="82"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="75"/>
-        <location filename="build/Desktop_Debug/qtsolda_autogen/include/ui_mainwindow.h" line="79"/>
+        <location filename="mainwindow.ui" line="68"/>
+        <location filename="build/Desktop_Debug/qtsolda_autogen/include/ui_mainwindow.h" line="77"/>
         <source>Open Database</source>
         <translation type="unfinished"></translation>
     </message>
